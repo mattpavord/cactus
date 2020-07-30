cafe850460612d45bc27482f70d81daea3f4dbcd
Matt Pavord <m.pavord@iwoca.co.uk>
2020-07-30 15:26:30 +0100

    Added skeleton homepage:

--- raincheck/views.py
+++ raincheck/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from raincheck.models import Plant, Customer
 
-# Create your views here.
+
+def index(request):
+    """View function for home page"""
+
+    num_plants = Plant.objects.all().count()
+    num_customers = Customer.objects.all().count()
+
+    context = {
+        'num_plants': num_plants,
+        'num_customers': num_customers,
+    }
+    return render(request, 'index.html', context=context)
