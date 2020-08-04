0810091f4af0355f84b46a8809fe71cf4be8c111
Matt Pavord <m.pavord@iwoca.co.uk>
2020-08-04 10:13:25 +0100

    Modified settings to allow heroku host

--- cactushugger/settings.py
+++ cactushugger/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0!wm%@m_ee+lre6qg5^j&q2ymx%+0(1jk#id=bi=idiywei^is'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '0!wm%@m_ee+lre6qg5^j&q2ymx%+0(1jk#id=bi=idiywei^is')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://murmuring-shelf-65530.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
