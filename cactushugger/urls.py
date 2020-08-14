ecd2efbb02ba82c6f7a92d5fabef1c4e6ef8f0fb
Matt Pavord <m.pavord@iwoca.co.uk>
2020-08-14 15:41:29 +0100

    Added basic form page

--- cactushugger/urls.py
+++ cactushugger/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from registration import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('raincheck/', include('raincheck.urls')),
     path('', RedirectView.as_view(url='raincheck/', permanent=True)),
+    path('registration/', core_views.signup),
 ]
