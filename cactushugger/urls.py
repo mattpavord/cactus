7211ea1301e913573945a7d229927e6f1d181a9f
Matt Pavord <m.pavord@iwoca.co.uk>
2020-11-19 19:21:23 +0000

    Banging landing page, add signup link on login page

--- cactushugger/urls.py
+++ cactushugger/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from registration import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('raincheck/', include('raincheck.urls')),
     path('signup/', core_views.signup, name='signup'),
+    path('', RedirectView.as_view(url='raincheck/', permanent=True)),
     path('login/', core_views.AdminLogin.as_view(), name='login'),
 ]
