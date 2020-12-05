57a45a33eb71da0479f27370673f3c193884711c
Matt Pavord <m.pavord@iwoca.co.uk>
2020-12-05 15:34:46 +0000

    Fixed geocoder api request to get country

--- raincheck/api_tasks.py
+++ raincheck/api_tasks.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import json
 import requests
 from django.conf import settings
@@ -13,18 +12,20 @@ def get_or_create_location_from_google_api(location_str: str) -> Location:
     url = f'https://maps.googleapis.com/maps/api/geocode/' \
           f'json?address={location_str}&key={GOOGLE_API_KEY}'
     response = requests.get(url)
+    if response.status_code != 200:
+        raise NameError(response.content)
+
     data = json.loads(response.content)['results'][0]
 
     country = ''
     region = location_str
     for address_component in data['address_components']:
-        if 'administrative_area_level_2' in address_component['types']:
-            region = address_component['long_name']
         if 'administrative_area_level_1' in address_component['types']:
+            region = address_component['long_name']
+        if 'country' in address_component['types']:
             country = address_component['long_name']
-    coordinate_data = data['geometry']['bounds']
-    latitude = (coordinate_data['northeast']['lat'] + coordinate_data['southwest']['lat']) / 2
-    longitude = (coordinate_data['northeast']['lng'] + coordinate_data['southwest']['lng']) / 2
+    latitude = data['geometry']['location']['lat']
+    longitude = data['geometry']['location']['lng']
 
     if Location.objects.filter(region=region).exists():
         location = Location.objects.get(region=region)
@@ -33,9 +34,9 @@ def get_or_create_location_from_google_api(location_str: str) -> Location:
     return location
 
 
-def check_weather(longitude, latitude):
+def check_weather(location: Location):
     # url = f"http://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}"  # city
     url = f"http://api.openweathermap.org/data/2.5/weather" \
-          f"?lat={latitude}&lon={longitude}&appid={WEATHER_API_KEY}"
+          f"?lat={location.latitude}&lon={location.longitude}&appid={WEATHER_API_KEY}"
     response = requests.get(url)
     print(response.__dict__)
