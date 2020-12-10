c4fa004992527883888ce6d9f926db666f0f2410
Matt Pavord <m.pavord@iwoca.co.uk>
2020-12-10 16:16:10 +0000

    Fix catalogue plant form bug

--- raincheck/forms.py
+++ raincheck/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class CataloguePlantForm(forms.Form):
     plant_name = forms.CharField(max_length=100)
-    latin_name = forms.CharField(max_length=100)
+    latin_name = forms.CharField(max_length=100, required=False)
     location = forms.CharField(max_length=100)
     country = forms.CharField(max_length=50, required=False)
     image = forms.ImageField()
